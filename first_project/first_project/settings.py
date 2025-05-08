721749e35601b2d6c5cdb03c57159ddf296b8f01
stanislavfor <stasposter@narod.ru>
2025-05-08 04:32:53 +0300

    added extends to project

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -52,10 +52,25 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'first_project.urls'
 
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'main_app/templates/main_app'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
